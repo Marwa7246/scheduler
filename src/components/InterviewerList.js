80466702d913da5e5a0b0b3a9fd9d8ec3a2b94f3
Marwa7246 <marwa.ragheb@gmail.com>
2020-09-01 15:41:09 -0400

    Build InterviwerList

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import InterviewerListItem from 'components/InterviewerListItem'
+import 'components/InterviewerList.scss'
+
+export default function InterviewerList(props) {
+  console.log('props', props);
+
+ const { interviewers, interviewer, setInterviewer }= props
+ console.log('interviewer', interviewer);
+
+//  const parsedInterviewers = interviewers.map(interviewer => <InterviewerListItem key={interviewer.id} name={interviewer.name} avatar={interviewer.avatar} /> )
+
+ 
+ const parsedInterviewers = interviewers.map(oneInterviewer => <InterviewerListItem {...oneInterviewer} selected = {interviewer === oneInterviewer.id} setInterviewer={setInterviewer}/>);
+
+
+ 
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header">Interviewer</h4>
+      <ul className="interviewers__list">{parsedInterviewers}</ul>
+    </section>
+  );
+}
