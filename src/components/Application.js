ef89e6c7181dd246282ee3ec921f6b3392d5af72
Marwa7246 <marwa.ragheb@gmail.com>
2020-09-02 09:14:54 -0400

    Add schedule to the app

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,8 @@ import React, { useState }  from "react";
 
 //import DayListItem from "components/DayListItem"
 import DayList from "components/DayList"
+import Appointment from "components/appointment";
+
 
 import "components/Application.scss";
 
@@ -24,12 +26,72 @@ import "components/Application.scss";
     },
   ];
 
+  const appointments = [
+    {
+      id: 1,
+      time: "12pm",
+    },
+    {
+      id: 2,
+      time: "1pm",
+      interview: {
+        student: "Lydia Miller-Jones",
+        interviewer: {
+          id: 1,
+          name: "Sylvia Palmer",
+          avatar: "https://i.imgur.com/LpaY82x.png",
+        }
+      }
+    },
+    {
+      id: 3,
+      time: "2pm",
+      interview: {
+        student: "Emily",
+        interviewer: { 
+          id: 2, 
+          name: "Tori Malcolm", 
+          avatar: "https://i.imgur.com/Nmx0Qxo.png" 
+        }
+      }
+    },
+    {
+      id: 4,
+      time: "3pm",
+      interview: {
+        student: "Maria",
+        interviewer: { 
+          id: 3, 
+          name: "Mildred Nazir", 
+          avatar: "https://i.imgur.com/T2WwVfS.png"
+        }
+      }
+    },
+    {
+      id: 5,
+      time: "4pm",
+      interview: {
+        student: "Nohad",
+        interviewer: { 
+          id: 4, 
+          name: "Cohana Roy", 
+          avatar: "https://i.imgur.com/twYrpay.jpg"
+        }
+      }
+    }
+  ];
 
 export default function Application(props) {
  
 
   const [day, setDay] = useState("Monday");
   console.log("dayState:", day);
+
+  const parsedAppointments = appointments.map(oneAppointment => {
+     console.log(oneAppointment);
+     return (<Appointment key={oneAppointment.id} {...oneAppointment} />)
+    })
+      
 
   return (
     <main className="layout">
@@ -57,6 +119,8 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {parsedAppointments}
+        <Appointment id="last" time="5pm" />
       </section>
     </main>
   );
--- src/components/appointment/index.js
+++ src/components/appointment/index.js
@@ -6,14 +6,14 @@ import Show from "components/appointment/Show";
 
 import 'components/appointment/styles.scss'
 
+
+
 export default function Appointment (props) {
 console.log(props)
-const { interviewer, student } = {...props.interviewer}
-const info = props.interviewer ? <Show {...interviewer} student={student}/> : <Empty/> 
-
+const { interviewer, student } = {...props.interview}
 
   return <article className="appointment">
     <Header time={props.time}/>
-    {info}
+    {props.interview ? <Show {...interviewer} student={student}/> : <Empty/>}
   </article>
 }
