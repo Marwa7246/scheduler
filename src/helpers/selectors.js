4e4874e940e1f6d41bfa14a57c93472fd0862cb1
Marwa7246 <marwa.ragheb@gmail.com>
2020-09-03 08:56:56 -0400

    Implement getInterviewer helper function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,8 +1,23 @@
-
- export function getAppointmentsForDay(state, day) {
+ 
+  export function getAppointmentsForDay(state, day) {
   const selectedDay= state.days.filter(ele => ele.name === day );
   return (selectedDay.length) ? selectedDay[0].appointments.map(ele => state.appointments[ele]) : [];
 }
 
 
-//console.log(getAppointmentsForDay(state, "Monday"))
+ export function getInterview(state, schedule) {
+  // console.log('from selector file:', schedule)
+  //  if (!schedule) return null
+  // const newSchedule = {...schedule, interviewer: interviewers[schedule.interviewer] }
+  // console.log('selector:', newSchedule)
+  //  return newSchedule
+
+  return (!schedule) ?  null: {...schedule, interviewer: state.interviewers[schedule.interviewer] }
+
+ 
+}
+
+
+//console.log(getInterview(state, schedule))
+// console.log(getAppointmentsForDay(state, 'Monday'))
+
