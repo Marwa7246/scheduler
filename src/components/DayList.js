f220a3743819137fe16c03c410a89096feaa1693
Marwa7246 <marwa.ragheb@gmail.com>
2020-09-03 11:47:36 -0400

    Retrieve interviewer data

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,11 +2,11 @@ import React from 'react';
 import DayListItem from 'components/DayListItem'
 
 export default function DayList(props) {
- //console.log(props);
+ console.log(props);
  const dayList = props.days.map(day => {
    return (
      <DayListItem
-            key={day.name}
+            key={day.id}
             name={day.name}
             spots={day.spots}
             selected= {day.name===props.day}            
