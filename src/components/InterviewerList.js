32c9e3b175885c4c520f5c43dba65e0936a73866
Marwa7246 <marwa.ragheb@gmail.com>
2020-09-07 14:21:02 -0400

    Add prop types test

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import InterviewerListItem from 'components/InterviewerListItem'
 import 'components/InterviewerList.scss'
 
+ 
 export default function InterviewerList(props) {
+  console.log(typeof props.interviewers);
+
+ 
+
   //console.log('props', props);
 
  const { interviewers, value, onChange }= props
@@ -11,6 +18,7 @@ export default function InterviewerList(props) {
 //  const parsedInterviewers = interviewers.map(interviewer => <InterviewerListItem key={interviewer.id} name={interviewer.name} avatar={interviewer.avatar} /> )
 
  
+
  const parsedInterviewers = interviewers.map(oneInterviewer => ( 
     <InterviewerListItem 
         key={oneInterviewer.id} 
@@ -29,3 +37,7 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
